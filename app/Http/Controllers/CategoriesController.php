b87029a69941441e2b8a5f1e54d705bd64d97758
NourShaqrawy <shkrawysaleh037@gmail.com>
2025-06-17 03:15:43 +0300

    .

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -35,6 +35,7 @@ class CategoriesController extends Controller
 
        return view('categories.index',[
         'categories' => $category,
+        'title'=> 'categories'
         
        ]);
     }
